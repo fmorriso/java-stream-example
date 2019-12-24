2af0f114791510a8b546405cb277138464698795
Fred Morrison <fpmorrison@verizon.net>
2019-12-24 09:15:25 -0500

    add default constructor

--- MinMax.java
+++ MinMax.java
@@ -1,7 +1,20 @@
-
+// custom class to hold a minimum and maximum whole number.
+// The original purpose was to use as the output from a complex java Stream / Teeing example
 public class MinMax
 {
 	private final Integer min;
+	private final Integer max;
+	
+	public MinMax() {
+		this(Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+	
+	public MinMax(Integer min, Integer max)
+	{
+		this.min = min;
+		this.max = max;
+	}
+	
 	public Integer getMin()
 	{
 		return min;
@@ -12,14 +25,4 @@ public class MinMax
 		return max;
 	}
 
-	private final Integer max;
-
-	public MinMax(Integer min, Integer max)
-	{
-
-		this.min = min;
-
-		this.max = max;
-
-	}
 }
