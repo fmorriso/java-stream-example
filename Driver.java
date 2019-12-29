f0faec8649daefe8ec53a6b339a4b8e838777ef5
Fred Morrison <fpmorrison@verizon.net>
2019-12-29 11:21:16 -0500

    refactor smallest, largest variable names

--- Driver.java
+++ Driver.java
@@ -14,17 +14,17 @@ public class Driver
 
 	public static void main(String[] args)
 	{
-		final int NUM_VALUES = 1000, MIN = 50, MAX = 100;
-		int smallest, largest;
-		double average, smallestDouble, largestDouble;
+		final int NUM_VALUES = 100, MIN = 50, MAX = 100;
+		int smallestWholeNumber, largestWholeNumber;
+		double average, smallestDecimalNumber, largestDecimalNumber;
 
 		int[] values1 = generateRandomIntArray(NUM_VALUES, MIN, MAX);
 		// System.out.println(Arrays.toString(values1));
 
 		// multiple passes over the same int array of values to find smallest and largest value
-		largest = Arrays.stream(values1).max().getAsInt();
-		smallest = Arrays.stream(values1).min().getAsInt();
-		System.out.format("1. smallest value = %d, largest value = %d%n", smallest, largest);
+		largestWholeNumber = Arrays.stream(values1).max().getAsInt();
+		smallestWholeNumber = Arrays.stream(values1).min().getAsInt();
+		System.out.format("1. smallest value = %d, largest value = %d%n", smallestWholeNumber, largestWholeNumber);
 
 		// generate Integer[] array so we can use Stream and Tee in a single (logical) pass over it.
 		// to find the smallest and largest value.
@@ -40,67 +40,67 @@ public class Driver
 								Collectors.maxBy(Comparator.naturalOrder()),
 								(Optional<Integer> a, Optional<Integer> b) -> new MinMax(a.orElse(Integer.MIN_VALUE), b.orElse(Integer.MAX_VALUE))));
 
-		largest = minmax.getMax().intValue();
-		smallest = minmax.getMin().intValue();
-		System.out.format("2. smallest value = %d, largest value = %d%n", smallest, largest);
+		largestWholeNumber = minmax.getMax().intValue();
+		smallestWholeNumber = minmax.getMin().intValue();
+		System.out.format("2. smallest value = %d, largest value = %d%n", smallestWholeNumber, largestWholeNumber);
 
 		List<Integer> values3 = generateRandomIntegerList(NUM_VALUES, MIN, MAX);
 		// System.out.println(values3);
-		largest = values3.stream()
+		largestWholeNumber = values3.stream()
 				.max(Comparator.naturalOrder())
 				.get()
 				.intValue();
-		smallest = values3.stream()
+		smallestWholeNumber = values3.stream()
 				.min(Comparator.naturalOrder())
 				.get()
 				.intValue();
-		System.out.format("3. smallest value = %d, largest value = %d%n", smallest, largest);
+		System.out.format("3. smallest value = %d, largest value = %d%n", smallestWholeNumber, largestWholeNumber);
 
 		// Get min/max in single pass using special built-in statistics class
 		IntSummaryStatistics stats3 = values3.stream()
 				.collect(IntSummaryStatistics::new, IntSummaryStatistics::accept, IntSummaryStatistics::combine);
-		smallest = stats3.getMin();
-		largest = stats3.getMax();
+		smallestWholeNumber = stats3.getMin();
+		largestWholeNumber = stats3.getMax();
 		average = stats3.getAverage();
-		System.out.format("4. smallest value = %d, largest value = %d, average = %.1f%n", smallest, largest, average);
+		System.out.format("4. smallest value = %d, largest value = %d, average = %.1f%n", smallestWholeNumber, largestWholeNumber, average);
 
 		List<Integer> values4 = generateRandomIntegerListExplicitCollector(NUM_VALUES, MIN, MAX);
 		// System.out.println(values4);
-		largest = values4.stream()
+		largestWholeNumber = values4.stream()
 				.max(Comparator.naturalOrder()) //  -> Optional<Integer>
 				.get() // -> Integer
 				.intValue(); // int
-		smallest = values4.stream()
+		smallestWholeNumber = values4.stream()
 				.min(Comparator.naturalOrder()) //  -> Optional<Integer>
 				.get() // Integer
 				.intValue(); // int
-		System.out.format("5. smallest value = %d, largest value = %d%n", smallest, largest);
+		System.out.format("5. smallest value = %d, largest value = %d%n", smallestWholeNumber, largestWholeNumber);
 
 		// use Stream.collect(supplier, accumulator, combiner) to gather statistics on the values
 		IntSummaryStatistics stats4 = values4.stream()
 				.collect(IntSummaryStatistics::new, IntSummaryStatistics::accept, IntSummaryStatistics::combine);
-		smallest = stats4.getMin();
-		largest = stats4.getMax();
+		smallestWholeNumber = stats4.getMin();
+		largestWholeNumber = stats4.getMax();
 		average = stats4.getAverage();
-		System.out.format("6. smallest value = %d, largest value = %d, average = %.1f%n", smallest, largest, average);
+		System.out.format("6. smallest value = %d, largest value = %d, average = %.1f%n", smallestWholeNumber, largestWholeNumber, average);
 
 		double[] values5 = generateRandomDoubleArray(NUM_VALUES, MIN, MAX);
 		// System.out.println(Arrays.toString(values5));
 		DoubleSummaryStatistics stats5 = Arrays.stream(values5)
 				.collect(DoubleSummaryStatistics::new, DoubleSummaryStatistics::accept, DoubleSummaryStatistics::combine);
-		smallestDouble = stats5.getMin();
-		largestDouble = stats5.getMax();
+		smallestDecimalNumber = stats5.getMin();
+		largestDecimalNumber = stats5.getMax();
 		average = stats5.getAverage();
-		System.out.format("7. smallest value = %.3f, largest value = %.3f, average = %.3f%n", smallestDouble, largestDouble, average);
+		System.out.format("7. smallest value = %.3f, largest value = %.3f, average = %.3f%n", smallestDecimalNumber, largestDecimalNumber, average);
 		
 		Double[] values6 = generateRandomBoxedDoubleArray(NUM_VALUES, MIN, MAX);
 		// use Stream.collect(supplier, accumulator, combiner) to gather statistics on the values
 		DoubleSummaryStatistics stats6 = Arrays.stream(values6)
 				.collect(DoubleSummaryStatistics::new, DoubleSummaryStatistics::accept, DoubleSummaryStatistics::combine);
-		smallestDouble = stats6.getMin();
-		largestDouble = stats6.getMax();
+		smallestDecimalNumber = stats6.getMin();
+		largestDecimalNumber = stats6.getMax();
 		average = stats6.getAverage();
-		System.out.format("8. smallest value = %.3f, largest value = %.3f, average = %.3f%n", smallestDouble, largestDouble, average);
+		System.out.format("8. smallest value = %.3f, largest value = %.3f, average = %.3f%n", smallestDecimalNumber, largestDecimalNumber, average);
 		
 	}
 
